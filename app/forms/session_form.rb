c34a162896be75127c29eee07d5f02864b39a85c
Vladimir Dulov <v.dulov@gmail.com>
2021-03-17 20:34:06 +0400

    Session controller implemented

---
+++ app/forms/session_form.rb
@@ -0,0 +1,24 @@
+class SessionForm
+  include ActiveModel::Model
+
+  attr_accessor(
+    :email,
+    :password,
+  )
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true
+  validate :user_valid?
+
+  def user
+    User.find_by(email: email)
+  end
+
+  private
+
+  def user_valid?
+    if user.blank? || !user.authenticate(password)
+      errors.add(:email, "email or password doesn't match")
+    end
+  end
+end
