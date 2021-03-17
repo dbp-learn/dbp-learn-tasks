957f971444c365cf4f3b538de26644ce405706f7
Vladimir Dulov <v.dulov@gmail.com>
2021-03-17 15:48:57 +0400

    code tidying up

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task do
     name
     description
-    author_id { association :manager, author_id: instance }
-    assignee_id { association :developer, assignee_id: instance }
+    author factory: :manager
+    assignee factory: :developer
     traits_for_enum(:state, ['new_task', 'in_development', 'in_qa', 'in_code_review', 'ready_for_release', 'released', 'archived'])
     expired_at
   end
