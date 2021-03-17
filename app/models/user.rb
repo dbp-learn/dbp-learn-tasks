41646caf2c0b7f9e92fadeb9f4df42c044b24673
Vladimir Dulov <v.dulov@gmail.com>
2021-03-17 11:02:58 +0400

    Model relations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
   has_secure_password
+
+  validates :first_name, presence: true, length: { minimum: 2, too_short: '%<count>s characters is the minimum allowed' }
+  validates :last_name, presence: true, length: { minimum: 2, too_short: '%<count>s characters is the minimum allowed' }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: '%<value>s invalid' }
 end
