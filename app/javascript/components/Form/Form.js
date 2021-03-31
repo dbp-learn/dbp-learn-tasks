e9877aa9558aa8b44b6baf51b872308e9b88485e
Vladimir Dulov <v.dulov@gmail.com>
2021-04-01 01:21:17 +0400

    Author select box implemented

--- app/javascript/components/Form/Form.js
+++ app/javascript/components/Form/Form.js
@@ -4,10 +4,15 @@ import { has } from 'ramda';
 
 import TextField from '@material-ui/core/TextField';
 
+import UserSelect from 'components/UserSelect';
+
 import useStyles from './useStyles';
 
 const Form = ({ errors, onChange, task }) => {
   const handleChangeTextField = (fieldName) => (event) => onChange({ ...task, [fieldName]: event.target.value });
+
+  const handleChangeSelect = (fieldName) => (user) => onChange({ ...task, [fieldName]: user });
+
   const styles = useStyles();
 
   return (
@@ -30,6 +35,15 @@ const Form = ({ errors, onChange, task }) => {
         required
         multiline
         margin="dense"
+      />
+      <UserSelect
+        label="Author"
+        value={task.author}
+        onChange={handleChangeSelect('author')}
+        isDisabled
+        isRequired
+        error={has('author', errors)}
+        helperText={errors.author}
       />
     </form>
   );
